58a174dc5e687faf58e7f8c50bae2c1785fae4db
Neelporos <sawkotalimahmud@gmail.com>
2022-04-05 03:06:06 -0700

    Question Added In Q&A Section

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2>This Is Blog</h2>
+            <h2 className='text-5xl text-center font-bold m-5'>Q & A</h2>
+            <h3 className='text-2xl text-center font-bold m-5'>Question: What is context api?</h3>
+            <p className='text-xl text-center font-bold m-5'>Answer: </p>
+            <h3 className='text-2xl text-center font-bold m-5'>Question: What is semantic tag?</h3>
+            <p className='text-xl text-center font-bold m-5'>Answer: </p>
+            <h3 className='text-2xl text-center font-bold m-5'>Question: What is difference between inline and block elements?</h3>
+            <p className='text-xl text-center font-bold m-5'>Answer: </p>
+
         </div>
     );
 };
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useReviews from '../../hooks/reviews';
-import HomeReviews from '../HomeReviews/HomeReviews';
+import ReviewCart from '../ReviewCart/ReviewCart';
 
 const Home = () => {
 
@@ -11,7 +11,7 @@ const Home = () => {
 
     return (
         <div>
-            <div className='grid grid-cols-2' >
+            <div className='grid grid-cols-1 lg:grid-cols-2' >
                 <div className='p-10 m-5'>
                     <h1 className='text-5xl mt-10 font-bold '>One Of The Best <br /> Handyman Service In All Over USA</h1>
                     <p className='mt-6'>Service pros inc has 20+years experience in usa.
@@ -23,7 +23,7 @@ const Home = () => {
                         we offer start-to finish solutions for every project.
                         no need to hire multiple companies or sub just bring us in
                         and let us give you free one stop shop experience.</p>
-                    <button className='mt-6 border-2 p-4 bg-white text-black hover:bg-black hover:text-stone-50 font-bold '>Call Now: 866-436-2012</button>
+                    <button className='mt-6 border-2 p-4 bg-white text-black hover:bg-black hover:text-stone-50 font-bold text-2xl '>Call Now: 866-436-2012</button>
                 </div>
                 <div>
                     <img className='mx-auto ' src='https://thumbs.dreamstime.com/b/multitasking-concept-handyman-different-tools-white-background-180022890.jpg' alt="" />
@@ -36,7 +36,7 @@ const Home = () => {
                 <div className='px-20 py-5 mx-auto max-w-7xl'>
                     <div className='grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-5 '>
                         {
-                            homeReview.map(review => <HomeReviews key={review._id} review={review}></HomeReviews>)
+                            homeReview.map(review => <ReviewCart key={review._id} review={review}></ReviewCart>)
                         }
                     </div>
 
