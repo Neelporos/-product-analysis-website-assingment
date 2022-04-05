e47fde4de96d664b930eec135dcd250c01437f19
Neelporos <sawkotalimahmud@gmail.com>
2022-04-05 07:39:21 -0700

    Blog Section Done

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -7,9 +7,9 @@ const Blog = () => {
             <h3 className='text-2xl text-center font-bold m-5'>Question: What is context api?</h3>
             <p className='text-xl text-center font-bold m-5'>Answer: Context api is one of the most powerful component or function that we can build in react for avoiding props drilling or dry " do not repeat your self" </p>
             <h3 className='text-2xl text-center font-bold m-5'>Question: What is semantic tag?</h3>
-            <p className='text-xl text-center font-bold m-5'>Answer: </p>
+            <p className='text-xl text-center font-bold m-5'>Answer: Semantic tag is mainly some tag that use in HTML CSS. every tag have a value or defination as like p tag defined as paragraph. </p>
             <h3 className='text-2xl text-center font-bold m-5'>Question: What is difference between inline and block elements?</h3>
-            <p className='text-xl text-center font-bold m-5'>Answer: </p>
+            <p className='text-xl text-center font-bold m-5'>Answer: 1.Inline material take only the required width.| Block Elements take the full width. <br></br> 2.Inline elements don't start in a new line | Block elements always start in a line.<br></br> 3.Inline elements allow other inline elements to sit behind.| Block elements doesn’t allow other elements to sit behind.<br></br> 4.Inline elements don’t have top and bottom margin | Block elements have top and bottom margin.</p>
 
         </div>
     );
--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Area, AreaChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, CartesianGrid, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
 
@@ -48,9 +48,9 @@ const Dashboard = () => {
                 <Line type="monotone" dataKey="investment" stroke="#8884d8" />
                 <Line type="monotone" dataKey="sell" stroke="#8884d8" />
                 <Line type="monotone" dataKey="revenue" stroke="#8884d8" />
-                <Line type="monotone" dataKey="month" stroke="#8884d8" />
+                <Line type="monotone" dataKey="month" stroke="#00008B" />
                 <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey="name" />
+                <XAxis dataKey="month" />
                 <YAxis />
                 <Tooltip></Tooltip>
             </LineChart>
@@ -60,14 +60,68 @@ const Dashboard = () => {
                 data={data}
             >
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
+                <XAxis dataKey="month" />
                 <YAxis />
                 <Tooltip />
                 <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
                 <Area type="monotone" dataKey="sell" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
                 <Area type="monotone" dataKey="revenue" stackId="1" stroke="#ffc658" fill="#ffc658" />
-                <Area type="monotone" dataKey="month" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                <Area type="monotone" dataKey="month" stackId="1" stroke="#00008B" fill="#ffc658" />
             </AreaChart>
+            <PieChart width={400} height={400}>
+                <Pie
+                    dataKey="investment"
+                    startAngle={180}
+                    endAngle={0}
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                />
+                <Pie
+                    dataKey="sell"
+                    startAngle={180}
+                    endAngle={0}
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                />
+                <Pie
+                    dataKey="revenue"
+                    startAngle={180}
+                    endAngle={0}
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                />
+                <Pie
+                    dataKey="month"
+                    startAngle={180}
+                    endAngle={0}
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                />
+                <Tooltip></Tooltip>
+            </PieChart>
+            <PieChart width={400} height={400}>
+                <Pie data={data} dataKey="month" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                <Pie data={data} dataKey="sell" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                <Pie data={data} dataKey="investment" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#00008B" label />
+                <Tooltip></Tooltip>
+            </PieChart>
         </div>
     );
 };
